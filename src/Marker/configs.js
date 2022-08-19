5d8975438e3ad327d48f82c9a4c2826e786e2e48
WRX <wrxpowered@gmail.com>
2022-08-19 14:22:06 +0800

    add config module

---
+++ src/Marker/configs.js
@@ -0,0 +1,10 @@
+
+const WORD_SPLIT_REG_EXP = /(\w+)|([\s]+)|(\S)/g;
+
+const TEXTUAL_ITEM_TYPE_ARRAY = ['paragraph', 'headline'];
+
+
+export {
+  WORD_SPLIT_REG_EXP,
+  TEXTUAL_ITEM_TYPE_ARRAY,
+}
